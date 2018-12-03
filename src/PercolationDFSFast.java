96effa23fce8224089682cdd1dbd52f09a8b849f
Sachin Jaishankar <sachin.jaishankar@duke.edu>
2018-12-03 13:44:28 -0500

    DFSFast fix

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -7,19 +7,30 @@ public class PercolationDFSFast extends PercolationDFS{
 	
 	@Override
 	public void updateOnOpen(int row, int col) {
+		int count = 0;
+		
+		
 		if(row == 0 && !isFull(row, col)) {
-			dfs(row, col);
+			count++;
 		}
 		
 		if(inBounds(row-1, col) && isFull(row-1, col)) {
-			dfs(row, col);
+			count++;
+		}
+		
+		if(inBounds(row+1, col) && isFull(row+1, col)) {
+			count++;
 		}
 		
 		if(inBounds(row, col-1) && isFull(row, col-1)) {
-			dfs(row, col);
+			count++;
 		}
 		
 		if(inBounds(row, col+1) && isFull(row, col+1)) {
+			count++;
+		}
+		
+		if(count > 0) {
 			dfs(row, col);
 		}
 		
